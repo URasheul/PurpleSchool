b127d4f141ea3253d8a253b8fd7ece6381254145
URasheul <ura_asheulov@mail.ru>
2024-02-24 19:51:12 +0300

    Второе домашнее задание. Модуль 5

---
+++ 5-languages.js
@@ -0,0 +1,12 @@
+'use strict'
+
+let language = 'en';
+
+switch(language){
+  case 'en' : console.log('Hello');
+  break;
+  case 'ru' : console.log('Привет');
+  break;
+  case 'de' : console.log('Guten Tag');
+  break;
+}
