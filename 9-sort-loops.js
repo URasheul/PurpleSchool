f0ba08915e2b88a9b5dc59b6c85a69a59fd1307b
URasheul <ura_asheulov@mail.ru>
2024-03-05 22:05:22 +0300

    Исправил замечания по ДЗ 9.9. - Циклы

--- 9-sort-loops.js
+++ 9-sort-loops.js
@@ -1,34 +1,22 @@
 'use strict'
 
-const arr = [-31, -15, -15, 1, 1, 0, -40, 5, 14, 23, 7, -3];
+const arr = [-31, -15, -41, -40, 0, -15, 1, 1, 0, -40, 5, 14, 23, 7, -3];
 
 function sort(array) {
-  let newArr = [].concat(array);
-  let newArrPositive = [].concat(array);
-  let sortArr = [];
-  let sortArrPositive = [];
-  for(let i ; newArr.length; i++ ){
-    i = 0;
-    let amount = newArr[i];
-    for(let j of newArr){
-      if( j < amount ){
-        amount = j;
-      }      
+  let initialArr = [...array];
+  let sortedArr = [];
+
+  for( let i = 0; i < array.length; i++){
+   let element = initialArr[0];
+    
+    for( let j of initialArr){
+      if(j < element){
+        element = j;
+      }
     }
-    newArr.splice(newArr.indexOf(amount), 1);
-    sortArr.push(amount);
+    initialArr.splice(initialArr.indexOf(element), 1);       
+    sortedArr.push(element)  
   }
-  for(let i ; newArrPositive.length; i++ ){
-    i = 0;
-    let amount = newArrPositive[i];
-    for(let j of newArrPositive){
-      if( j > amount ){
-        amount = j;
-      }      
-    }
-    newArrPositive.splice(newArrPositive.indexOf(amount), 1);
-    sortArrPositive.push(amount);
-  }
-  return `По возрастанию: ${sortArr}
-  По убыванию: ${sortArrPositive}`;
+  return sortedArr
 }
+
