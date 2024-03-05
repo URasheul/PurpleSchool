0671ebab319ee0aa549e23d0471f528bd69a4bb9
URasheul <ura_asheulov@mail.ru>
2024-03-05 23:06:01 +0300

    Добавил еще несколько вариантов решения задачи

--- 9-sort-loops.js
+++ 9-sort-loops.js
@@ -1,13 +1,13 @@
 'use strict'
 
-const arr = [-31, -15, -41, -40, 0, -15, 1, 1, 0, -40, 5, 14, 23, 7, -3];
+const arr = [-31, -40, 5, 13, 0 , 0 , -33,  5, 14, 23, 7, -3];
 
 function sort(array) {
   let initialArr = [...array];
   let sortedArr = [];
-
+  
   for( let i = 0; i < array.length; i++){
-   let element = initialArr[0];
+    let element = initialArr[0];
     
     for( let j of initialArr){
       if(j < element){
@@ -19,4 +19,51 @@ function sort(array) {
   }
   return sortedArr
 }
+
+
+/* Version 2 */
+
+function sort2(array) {
+  let initialArr = [...array];
+  
+  for( let i = 0; i < array.length; i++){
+    let element = initialArr[i];
+    let minElementIndex = i;
+    
+    for( let j = i; j < array.length; j++){
+      if(initialArr[j] < element){
+        element = initialArr[j];
+        minElementIndex = j;
+      }
+    }
+    initialArr[minElementIndex] = initialArr[i];
+    initialArr[i] = element;
+    
+  }
+  
+  return initialArr
+}
+
+
+
+/* Version 3
+тут отсутствует цикл в цикле*/
+
+
+function sort3(array) {
+  let initialArr = [...array];
+  let sortedArr = [];
+  
+  for( let i = 0; i < array.length; i++){
+    sortedArr.push(Math.min(...initialArr))    
+    initialArr.splice(initialArr.indexOf(sortedArr[ sortedArr.length -1 ]), 1);       
+    
+  }
+  return sortedArr
+}
+
+
+
+
+
 
