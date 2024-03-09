6a3c130aaa1ccc3ef826f3d843d4de9a405f44e4
URasheul <ura_asheulov@mail.ru>
2024-03-09 14:28:38 +0300

    Поправил замечания

--- 3-birthday.js
+++ 3-birthday.js
@@ -1,13 +1,14 @@
 'use strict'
 
-const userBirthday = '2009-01-04'
+const userBirthday = '2009-08-19'
 
 function ageValidation(age){
-    const userAge = new Date(age)    
+    const necessaryAge = 14 * 365 * 24 * 60 * 60 * 1000;
+    const userAge = new Date(age)       
     const now = new Date();
-    const isMoreThan14 = new Date(now.getTime() - userAge.getTime()).getFullYear() - 1970; 
+    const isMoreThan14 = now - userAge > necessaryAge;
     
-    return isMoreThan14 > 14
+    return isMoreThan14 
 }
 
 
